0011db4fbe79dc3bfb229ccfd1283b4472f122ba
Daniel15cs <danila.kirichenko.12@gmail.com>
2025-10-03 12:11:57 +0200

    v2

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "<h1> Hello world from Azure with python by Danylo Kyrychenko for lab6 using git!V1</h1>"
- # ""<p> Виконав Кириченко Данило Романович</p>"
+    return "<h1> Hello world from Azure with python!V2</h1><p> Виконав Кириченко Данило Романович</p>"
 
 if __name__ == '__main__':
     app.run()
