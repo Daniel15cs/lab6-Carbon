95902495a84e4faec1702b98be26d86e5c827f0c
Daniel15cs <danila.kirichenko.12@gmail.com>
2025-10-03 11:56:12 +0200

    version definition: v1

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "<h1> Hello world from Azure with python by Danylo Kyrychenko for lab6 using git and nvim btw! </h1>"
+    return "<h1> Hello world from Azure with python by Danylo Kyrychenko for lab6 using git!V1</h1>"
+ # ""<p> Виконав Кириченко Данило Романович</p>"
 
 if __name__ == '__main__':
     app.run()
